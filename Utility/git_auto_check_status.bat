40e3c5ffff62f21207f6107f5c1069ebd47c32db
Vern LH <120342118@qq.com>
2021-11-27 01:13:52 +0800

    修改自动提交文件格式

--- Utility/git_auto_check_status.bat
+++ Utility/git_auto_check_status.bat
@@ -1,18 +1,25 @@
 @echo off
 
 ::进入本地库目录 切换磁盘时需要额外增加参数 "/d" 表示切换到新磁盘
-cd /d D:\workspace\person\local_repositoies
+cd /d E:\WorkStation\PersonRepositories
 
-echo start %cd%
+echo start auto check local repositories
+echo.
 
 ::遍历同级目录 非仓库目录也会遍历
-for /f %%i in ('"dir /ad/b/on %cd%"') do (
-echo %%i
-::进入文件夹
+for /f %%j in ('"dir /ad/b/on %cd%"') do (
+cd %%j
+
+::进入子目录遍历
+for /f %%i in ('"dir /ad/b/on %cd%\%%j"') do (
+::进入文件夹 执行git status
 cd %%i
+echo ====================%%j %%i====================
 git status
 cd ..
-echo %%i end
 echo.
+)
+
+cd ..
 )
 pause
--- Utility/git_auto_update.bat
+++ Utility/git_auto_update.bat
@@ -1,19 +1,25 @@
 @echo off
 
-::进入本地库目录
-D:
-cd workspace\person\local_repositoies
+::进入本地库目录 切换磁盘时需要额外增加参数 "/d" 表示切换到新磁盘
+cd /d E:\WorkStation\PersonRepositories
 
-echo start %cd%
+echo start auto pull remote repositories
+echo.
 
 ::遍历同级目录 非仓库目录也会遍历
-for /f %%i in ('"dir /ad/b/on %cd%"') do (
-echo %%i
-::进入文件夹
+for /f %%j in ('"dir /ad/b/on %cd%"') do (
+cd %%j
+
+::进入子目录遍历
+for /f %%i in ('"dir /ad/b/on %cd%\%%j"') do (
+::进入文件夹 执行git status
 cd %%i
+echo ====================%%j %%i====================
 git pull
 cd ..
-echo %%i end
 echo.
+)
+
+cd ..
 )
 pause
